22eb4cd7ec924523bf58f236f8aab8b308c358dc
Mardonis Aves Bezerra <mardonisgp@gmail.com>
2021-06-08 00:56:23 -0300

    liste-tarefas

--- src/app/listatarefa/listatarefa.component.ts
+++ src/app/listatarefa/listatarefa.component.ts
@@ -1,15 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {faListAlt , faHome , faBalanceScale} from '@fortawesome/free-solid-svg-icons'
+import  baseURl  from '../baseURl/baseUrl'
+import { Tarefas } from '../models/Tarefas';
 @Component({
   selector: 'app-listatarefa',
   templateUrl: './listatarefa.component.html',
   styleUrls: ['./listatarefa.component.css']
 })
 export class ListatarefaComponent implements OnInit {
+  id: any
+  listaTarefas: any
   faHome=faHome
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private http:HttpClient) { 
+    this.id = localStorage.getItem('id') || ""
   }
 
+  ngOnInit(): void {
+    this.getTarefas(this.id);
+  }
+
+  /* Este metodo recebe um parametro id
+    o id a gente obtem do localStorage
+  */
+  getTarefas(id:any){
+    /* Agora vamos fazer a chamada do metodo do back-end*/
+   return this.http.get<Tarefas>(`${baseURl}tarefas`,id)
+   .subscribe( (data => {
+      this.listaTarefas = data
+      console.log(this.listaTarefas)
+
+   }),err => {
+      console.log(err)
+   }
+
+   )
+  }
 }
