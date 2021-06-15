a455b9056c8184f7183404e9add833ffb104bf4b
Mardonis Aves Bezerra <mardonisgp@gmail.com>
2021-06-14 21:02:07 -0300

    build

--- src/app/baseURl/baseUrl.ts
+++ src/app/baseURl/baseUrl.ts
@@ -1,3 +1,4 @@
- const   baseUrl:string =  'https://backtolist.herokuapp.com/'
+ const   baseUrl:string =  'http://backtolist.herokuapp.com/'
+// const   baseUrl:string =  'http://localhost:3000/'
 export default   baseUrl
     
