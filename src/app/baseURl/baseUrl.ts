e76f543fdda9379192b968c083111b3d51845d22
Mardonis Aves Bezerra <mardonisgp@gmail.com>
2021-07-16 09:31:47 -0300

    base-url

--- src/app/baseURl/baseUrl.ts
+++ src/app/baseURl/baseUrl.ts
@@ -1,5 +1,5 @@
-//const   baseUrl:string =  'https://backtolist.herokuapp.com/'
-const   baseUrl:string =  'http://localhost:8080/'
+const   baseUrl:string =  'https://backtolist.herokuapp.com/'
+//const   baseUrl:string =  'http://localhost:8080/'
 
 export default   baseUrl
     
